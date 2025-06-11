f6475179c2e45c05be5a082595349bd1595598e5
VelogaWhite <gunyok2515@gmail.com>
2025-06-12 02:52:55 +0700

    Improve some algorithm
    selist and linelists's algorithm

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -1,4 +1,5 @@
 def printscreen(data_list):
+    
     print_line = "    ".join([f"{item:>6}" for item in data_list])
     print(print_line)
     
@@ -23,16 +24,21 @@ def arithmetic_arranger(problems, show_answers=False):
             if len(x[0]) and len(x[2]) > 4:
                 return 'Error: Numbers cannot be more than four digits.'
             #and split one into each line
+
+            max_len = max(len(x[0]), len(x[2]))
+            column_width = max_len + 2 # +2 for operator and space
+
             fnlist.append(x[0])
-            selist.append(x[1]+ " " +x[2])
-            lineslist.append("-" *(len(selist[count]))) #and - to the lineslist according to len of selist
+            
+            selist.append(x[1]+ (" "*(column_width - len(x[2]) - 1) +x[2]))
+            lineslist.append("-" *(column_width)) #and - to the lineslist according to len of selist
             try:
                 answer = int(x[0]) + int(x[1] + x[2])
             except TypeError and ValueError:
                 return 'Error: Numbers must only contain digits.'
             answerlist.append(answer)
             count += 1
-  
+    
     printscreen(fnlist)
     printscreen(selist)
     printscreen(lineslist)
@@ -43,4 +49,6 @@ def arithmetic_arranger(problems, show_answers=False):
 
     return problems
 
-print(f'\n{arithmetic_arranger(["3 + 608", "3801 + 2000", "45 + 43", "123 + 49"],True)}')
+
+print("------------------------------- ")
+arithmetic_arranger(["3801 - 2", "123 + 49"],True)
