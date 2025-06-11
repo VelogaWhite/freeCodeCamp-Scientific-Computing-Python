bda2241517607e3d2fced384464ea12be6a13ae5
VelogaWhite <gunyok2515@gmail.com>
2025-06-12 03:24:37 +0700

    Updated arithmetic_arranger 1.0.py
    This version is finished version, it work completely fine.
    Change:
     - Decide to remove printscreen function due to doesn't need to use
     - Change how input each digit to each list and calculate the column_width
     - added arranged_output instead of printscreen function
     - improve most of algorithm to work better and use less lines[
        - Operator Checker
        - how each number append in fnlist, selist and answerlist.
    ]
     - fix bugs[
        - limit of problem is 4 instead of 5
        - fix numbers digit checker brings "and" instead of or
        - try and except bring TypeError and ValueError instead of (TypeError, ValueError)
     ]

--- arithmetic-formatter/arithmetic_arranger.py
+++ arithmetic-formatter/arithmetic_arranger.py
@@ -1,54 +1,47 @@
-def printscreen(data_list):
-    
-    print_line = "    ".join([f"{item:>6}" for item in data_list])
-    print(print_line)
-    
 def arithmetic_arranger(problems, show_answers=False):
-    if len(problems) > 4:
+    if len(problems) > 5:
         return 'Error: Too many problems.'
     fnlist = [] #first line
     selist = [] #second line
     lineslist = [] #third line
     answerlist = [] 
-    count = 0
     for prob in problems:
 
-        if '*'  in prob:
+        if '*'  in prob or '/' in prob:
             return "Error: Operator must be '+' or '-'."
-        elif '/' in prob:
-            return "Error: Operator must be '+' or '-'."
-        
         else:
             #split each problem into 3 sections
             x =prob.split()
-            if len(x[0]) and len(x[2]) > 4:
+            if len(x[0]) > 4 or len(x[2]) > 4:
                 return 'Error: Numbers cannot be more than four digits.'
             #and split one into each line
 
             max_len = max(len(x[0]), len(x[2]))
             column_width = max_len + 2 # +2 for operator and space
 
-            fnlist.append(x[0])
+            fnlist.append(f"{x[0]:>{column_width}}")
             
-            selist.append(x[1]+ (" "*(column_width - len(x[2]) - 1) +x[2]))
+            selist.append(f"{x[1]}{' ' * (column_width - len(x[2]) - 1)}{x[2]}")
             lineslist.append("-" *(column_width)) #and - to the lineslist according to len of selist
             try:
                 answer = int(x[0]) + int(x[1] + x[2])
-            except TypeError and ValueError:
+            except (TypeError, ValueError):
                 return 'Error: Numbers must only contain digits.'
-            answerlist.append(answer)
-            count += 1
-    
-    printscreen(fnlist)
-    printscreen(selist)
-    printscreen(lineslist)
-    if show_answers == True:
-        printscreen(answerlist)
-                
+            answerlist.append(str(f"{answer:>{column_width}}"))
+       
+   
+    arranged_output = []
+    arranged_output.append("    ".join(fnlist))
+    arranged_output.append("    ".join(selist))
+    arranged_output.append("    ".join(lineslist))
+
+    if show_answers:
+        arranged_output.append("    ".join(answerlist))
 
 
-    return problems
+
+    return "\n".join(arranged_output)
 
 
 print("------------------------------- ")
-arithmetic_arranger(["3801 - 2", "123 + 49"],True)
+print(f'\n{arithmetic_arranger(["3255 + 6985", "3801 - 2", "45 + 43", "123 + 49"], True)}')
